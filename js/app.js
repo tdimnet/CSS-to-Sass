84ff3cb21e4776a440a0334d7a39b9b11a7d3bbd
Thomas Dimnet <Thomas.dimnet@gmail.com>
2016-11-30 19:28:25 +0100

    Last modifications before submit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-
-/*
-  To Do later (in order to have unobtrusive JS):
-    1. Create the variables
-    2. Create the function
-    3. Add the elements to the DOM with JS.
-    4. Run the function
-*/
 var $navList = $('.main-nav ul');
 
 
